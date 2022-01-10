a66db538eb5c2fed4dd88c0376c470e75b3bcd41
John Koster <john@stillat.com>
2022-01-09 20:03:29 -0600

    Disable duplicate check for now

--- server/src/diagnostics/documentHandlers/coreDocHandlers.ts
+++ server/src/diagnostics/documentHandlers/coreDocHandlers.ts
@@ -1,12 +1,10 @@
 import { IDocumentDiagnosticsHandler } from '../documentHandler';
-import DuplicateNeighboringTags from './duplicateNeighboringTags';
 import EmptyDocumentHandler from './emptyDocumentHandler';
 import FrontMatterHandler from './frontMatterHandler';
 
 const CoreDocumentHandlers:IDocumentDiagnosticsHandler[] = [
 	EmptyDocumentHandler,
 	FrontMatterHandler,
-	DuplicateNeighboringTags
 ];
 
 export {CoreDocumentHandlers};
