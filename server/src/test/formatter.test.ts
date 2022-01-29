605e600030e5cce2a8ad2ac643db4d656939cf1c
John Koster <john@stillat.com>
2022-01-29 14:08:17 -0600

    Adds coverage for #17

--- server/src/test/formatter.test.ts
+++ server/src/test/formatter.test.ts
@@ -50,6 +50,75 @@ const input = `{{#
         assert.strictEqual(format(input), expected);
     });
 
+	test('it does not double up', () => {
+		const initial = `{{# Page title #}}
+		<title>
+			{{ yield:seo_title }}
+			{{ seo_title ? seo_title : title }}
+			{{ seo:title_separator ? seo:title_separator : " &#124; " }}
+			{{ seo:change_page_title where="collection:{collection}" }}
+				{{ if what_to_add == 'collection_title' }}
+					{{ collection:title }}
+				{{ elseif what_to_add == 'custom_text' }}
+					{{ custom_text }}
+				{{ /if }}
+				{{ seo:title_separator ? seo:title_separator : " &#124; " }}
+			{{ /seo:change_page_title }}
+			{{ seo:site_name ? seo:site_name : config:app:name }}
+		</title>
+		
+		{{# Page description #}}
+		{{ if seo_description }}
+			  <meta name="description" content="{{ seo_description }}">
+		{{ elseif seo:collection_defaults }}
+			  <meta name="description" content="{{ partial:snippets/fallback_description }}">
+		{{ /if }}
+		
+		
+		{{# Some other comments #}}
+		{{ if seo_description }}
+			<meta name="description" content="{{ seo_description }}">
+		{{ elseif seo:collection_defaults }}
+			<meta name="description" content="{{ partial:snippets/fallback_description }}">
+		{{ /if }}`;
+const output = `{{# Page title #}}
+<title>
+    {{ yield:seo_title }}
+    {{ seo_title ? seo_title : title }}
+    {{ seo:title_separator ? seo:title_separator : " &#124; " }}
+    {{ seo:change_page_title where="collection:{collection}" }}
+        {{ if what_to_add == 'collection_title' }}
+            {{ collection:title }}
+        {{ elseif what_to_add == 'custom_text' }}
+            {{ custom_text }}
+         {{ /if }}
+        {{ seo:title_separator ? seo:title_separator : " &#124; " }}
+    {{ /seo:change_page_title }}
+    {{ seo:site_name ? seo:site_name : config:app:name }}
+</title>
+
+{{# Page description #}}
+{{ if seo_description }}
+    <meta name="description" content="{{ seo_description }}">
+
+{{ elseif seo:collection_defaults }}
+    <meta name="description" content="{{ partial:snippets/fallback_description }}">
+{{ /if }}
+
+
+{{# Some other comments #}}
+{{ if seo_description }}
+    <meta name="description" content="{{ seo_description }}">
+
+{{ elseif seo:collection_defaults }}
+    <meta name="description" content="{{ partial:snippets/fallback_description }}">
+{{ /if }}`;
+		assert.strictEqual(format(initial), output);
+		for (let i = 0; i <= 10; i++) {
+			assert.strictEqual(format(output), output);
+		}
+	});
+
 	test('it does not remove unless else', () => {
 const output = `<body class="flex flex-col min-h-screen bg-white selection:bg-primary selection:text-white {{ unless segment_1 }}home-{{else}}page-{{ /unless }}content">`;
 		assert.strictEqual(
