41ff62ed602dc0f48b45f04f473b62bb52703c16
John Koster <john@stillat.com>
2022-11-22 17:53:22 -0600

    Improves collection field scope injections

--- server/src/antlers/tags/core/collection/augmentCollectionScope.ts
+++ server/src/antlers/tags/core/collection/augmentCollectionScope.ts
@@ -1,6 +1,7 @@
 import { AntlersNode } from '../../../../runtime/nodes/abstractNode';
 import { checkSymbolForScopeAndAlias } from "../../../scope/factories/listFactory";
 import { Scope } from '../../../scope/scope';
+import { ICollectionContext } from '../../../types';
 import { makeCollectionVariables } from "../../../variables/collectionVariables";
 import { getCollectionBlueprintFields } from "./utils";
 
@@ -14,7 +15,14 @@ export function augmentCollectionScope(node: AntlersNode, scope: Scope): Scope {
     }
 
     if (node.reference != null) {
-        const fields = getCollectionBlueprintFields(node, scope);
+        const fields = getCollectionBlueprintFields(node, scope),
+            collRef = node.reference as ICollectionContext;
+        
+        if (fields.length == 0 && collRef.collectionNames.length > 0) {
+            fields.push({ blueprintName: collRef.collectionNames[0], displayName: 'Title', import: null, instructionText: '', maxItems: null, name: 'title', type: 'string', refFieldSetField: null, sets: [] });
+            fields.push({ blueprintName: collRef.collectionNames[0], displayName: 'Slug', import: null, instructionText: '', maxItems: null, name: 'slug', type: 'string', refFieldSetField: null, sets: [] });
+            fields.push({ blueprintName: collRef.collectionNames[0], displayName: 'Date', import: null, instructionText: '', maxItems: null, name: 'date', type: 'string', refFieldSetField: null, sets: [] });
+        }
 
         checkSymbolForScopeAndAlias(node, scope, fields);
     }
